ab3456b3d5e044c490101bc5c372b3f500d64209
Rajhut Fernandez <rajhutfn@gmail.com>
2023-09-22 18:40:48 -0500

    my first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/hour", (req, res) => {
+    const now = new Date();
+    const hour = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+    res.send({message: hour});
+});
+
+app.listen(6001, () => {
+    console.log("Servicio iniciado en http://localhost:6001");
+});
